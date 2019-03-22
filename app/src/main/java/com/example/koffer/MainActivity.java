391133d5d0ebaa323a3a726ce901a63f2f2e3e31
Dani Pando Moreno <34524302+RedWolf3121@users.noreply.github.com>
2019-03-22 19:21:42 +0100

    Main (xml+java) + LoginActivity(xml) + RegisterActivity(xml)

--- app/src/main/java/com/example/koffer/MainActivity.java
+++ app/src/main/java/com/example/koffer/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.koffer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.koffer.view.LoginActivity;
+import com.example.koffer.view.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registro;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registro = findViewById(R.id.btnRegistro);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        login = findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
