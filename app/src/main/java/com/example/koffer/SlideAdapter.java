e42c005c6448917e8b7b1570c6eb3bd270732542
nrufino15 <nrufino@elpuig.xeill.net>
2019-04-12 17:50:11 +0200

    Welcome slide acabado y totalmente funcional.

--- app/src/main/java/com/example/koffer/SlideAdapter.java
+++ app/src/main/java/com/example/koffer/SlideAdapter.java
@@ -2,11 +2,14 @@ package com.example.koffer;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class SlideAdapter extends PagerAdapter {
 
@@ -58,14 +61,29 @@ public class SlideAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
-        return view == (RelativeLayout) o;
+        return view == (ConstraintLayout) o;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-        View view = layoutInflater.inflate(R.id.slide_layout, container, false);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImageView = (ImageView) view.findViewById(R.id.slide_img);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_header);
+        TextView slideDesc = (TextView) view.findViewById(R.id.slide_desc);
+
+        slideImageView.setImageResource(slide_img[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDesc.setText(slide_descs[position]);
+
+        container.addView(view);
         return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout)object);
     }
 }
