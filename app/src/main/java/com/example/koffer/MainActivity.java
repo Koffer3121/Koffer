c685eb9e97eeedcced7da79bb5914a6a421c2711
nrufino15 <nrufino@elpuig.xeill.net>
2019-04-26 19:38:57 +0200

    Navigation bar added

--- app/src/main/java/com/example/koffer/MainActivity.java
+++ app/src/main/java/com/example/koffer/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(MainActivity.this, SlideActivity.class);
         startActivity(intent);
+        finish();
     }
 }
