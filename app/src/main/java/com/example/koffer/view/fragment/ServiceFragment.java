f2c1c3056c450d8c7d8dcd1593bc8ce9c652e578
Dani Pando Moreno <34524302+RedWolf3121@users.noreply.github.com>
2019-05-08 18:43:07 +0200

    co

--- app/src/main/java/com/example/koffer/view/fragment/ServiceFragment.java
+++ app/src/main/java/com/example/koffer/view/fragment/ServiceFragment.java
@@ -15,6 +15,8 @@ public class ServiceFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        
         return inflater.inflate(R.layout.fragment_service, container, false);
     }
 }
