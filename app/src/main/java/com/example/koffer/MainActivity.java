845f16c7b603d3ea119783656e4e0b3c664e2c4c
nrufino15 <nrufino@elpuig.xeill.net>
2019-05-08 20:24:19 +0200

    Comments in MainActivity added

--- app/src/main/java/com/example/koffer/MainActivity.java
+++ app/src/main/java/com/example/koffer/MainActivity.java
@@ -33,4 +33,21 @@ public class MainActivity extends AppCompatActivity {
 //    getReference().child("suitcase").child(suitcaseKey).setValue(suitcase);
 //    Ref().child("user-suicase").child(uid).child(suitcaseKey).setValue(true);
 //     }
+    //uid = getCurrent.guid()
+
+//        view.findViewById();
+
+// this   =>   getActivity()
+
+        /*
+            nclick(){
+                saco los dato
+                long =
+                lat  =
+                canti =
+                String suitcaseKey = getReference().push().getKey();
+                getReference().child("suitcase").child(suitcaseKey).setValue(suitcase);
+                Ref().child("user-suicase").child(uid).child(suitcaseKey).setValue(true);
+            }
+         */
 }
